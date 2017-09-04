2e70ed1888ec90cc363a311b6af5fdd1355dad99
AEIENG\hhofer <hhofer@aeieng.com>
2017-09-04 17:46:35 -0400

    9/4

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import promise from 'redux-promise';
 
 import PostsIndex from './components/posts_index';
 import PostsNew from './components/posts_new';
-
+import PostsShow from './components/posts_show';
 
 import reducers from './reducers';
 
@@ -20,7 +20,8 @@ ReactDOM.render(
       <div>
         <Switch>
           <Route path="/posts/new" component={PostsNew} />
-          <Route path="/" component={PostsIndex} />
+            <Route path="/posts/:id" component={PostsShow} />
+            <Route path="/" component={PostsIndex} />
         </Switch>
       </div>
     </BrowserRouter>
